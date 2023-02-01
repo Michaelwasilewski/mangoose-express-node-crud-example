9ec9fdac19d763cbc8533681311a1de350d63672
Michael Wasilewski <michael_grimestad94@hotmail.com>
2023-02-01 12:42:48 +0100

    adding new routes

--- app.js
+++ app.js
@@ -40,6 +40,12 @@ db.once('open', function() {
     console.log("We're connected to the database <3 <3 !");
 });
 
+app.get('/', (req, res) => {
+    res.send("Hello i am a response hihi");
+});
+app.get('*', (req, res) => {
+    res.send("Hello motherfucker i am a response hihi");
+});
 // create a new device
 app.post('/createcar', async (req, res) => {
     console.log(req.body)
